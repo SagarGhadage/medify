c6780fe7f6966495bc74a995630b988059a3e90f
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-05-09 20:42:57 +0530

    Home

---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+export default function HomePage({ children }) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
