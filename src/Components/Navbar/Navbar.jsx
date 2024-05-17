092132c96ea663211524a879f9e6ee312130e7aa
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-05-17 09:24:32 +0530

    Navbar modifie

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { Children } from 'react'
 import style from './Navbar.module.css'
 import Logo from '../Logo/Logo'
 import Button, { btnVarient } from '../Button/Button'
 
-export default function Navbar() {
+export default function Navbar({ children,transperent=0 }) {
     return (
-        <div className={style.navbar}>
-            <Logo/>
-            <Button varient={btnVarient.LIGHTBLUE} handleClick={()=>{console.log('My Bookings btn')}} btnText={'My Bookings'}></Button>
+        <div className={transperent ? style.transperent : style.navbar}>
+            <Logo />
+            {children}
+            <div className={style.navlist}>
+                <span className={style.span}>Find Doctors</span>
+                <span className={style.span}>Hospitals</span>
+                <span className={style.span}>Medicines</span>
+                <span className={style.span}>Surgeries</span>
+                <span className={style.span}>Software for Provider</span>
+                <span className={style.span}>Facilities</span>
+                <Button varient={btnVarient.LIGHTBLUE} handleClick={() => { console.log('My Bookings btn') }} btnText={'My Bookings'}></Button>
+            </div>
         </div>
     )
 }
