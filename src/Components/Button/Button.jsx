7575c88093cbd71c848f012b09edc8b108dc539d
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-05-17 09:17:50 +0530

    Btn implimented

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from "./Button.module.css"
-function Button({ varient, handleClick, btnText, children ,type}) {
+function Button({ varient, handleClick, btnText, children, type }) {
   console.log(varient)
   switch (varient) {
     // case btnVarient.GREEN:
@@ -12,19 +12,19 @@ function Button({ varient, handleClick, btnText, children ,type}) {
     // )
     case btnVarient.LIGHTBLUE:
       return (
-        <button className={style.LIGHTBLUE} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+        <button className={style.LIGHTBLUE} onClick={handleClick}>{children} {btnText?btnText:""} </button>
       )
     case btnVarient.BLACK:
       return (
-        <button type={type} className={style.BLACK} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+        <button type={type} className={style.BLACK} onClick={handleClick}>{children}{btnText?btnText:"btn"} </button>
       )
     default: return (
       <button className={style.defaultBtn} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
-    ) 
+    )
   }
 
 }
 export const btnVarient = Object.freeze({
-  LIGHTBLUE:'LIGHTBLUE', BLACK: "BLACK",
+  LIGHTBLUE: 'LIGHTBLUE', BLACK: "BLACK",
 })
 export default Button
