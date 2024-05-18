da7e11c75dbf1cde6da4cdbec455fedb4d1397cb
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-05-18 23:19:45 +0530

    static data=[slots,faq,specialists]added

---
+++ src/data/data.js
@@ -0,0 +1,72 @@
+export const specialists = [
+    { id: 1, name: "Dr. Lesley Hull", specialisation: 'Medicine' },
+    { id: 2, name: 'Dr. Ahmad Khan', specialisation: 'Neurologist' },
+    { id: 3, name: "Dr. Heena Sachdeva", specialisation: 'Orthopadics' },
+    { id: 4, name: "Dr. Ankur Sharma", specialisation: 'Medicine' },
+    { id: 5, name: "Dr. Lesley Hull", specialisation: 'Medicine' },
+    { id: 6, name: "Dr. Ahmad Khan", specialisation: 'Orthopadics' },
+    { id: 7, name: "Dr. Lesley Hull", specialisation: 'Medicine' },
+    { id: 8, name: "Dr. Ankur Sharma", specialisation: 'Medicine' },
+    { id: 9, name: "Dr. Lesley Hull", specialisation: 'Orthopadics' },
+]
+export let offers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const dataFAQ = [
+    {
+        que: 'Why we are different from others?', ans: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.'
+    },
+    {
+        que: 'Why choose our medical for your family?', ans: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+      malesuada lacus ex, sit amet blandit leo lobortis eget.`
+    },
+    {
+        que: 'Trusted & experience senior care & love', ans: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.'
+    }
+    ,
+    {
+        que: 'How to get appointment for emergency cases?', ans: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.'
+    }
+]
+export let slots = [
+    {date:'2024-05-10', day: 'Today', slots: 3 },
+    {date:'2024-05-11', day: 'Tommorow', slots: 9 },
+    {date:'2024-05-12', day: 'Wed', slots: 6 },
+    {date:'2024-05-13', day: 'Thu', slots: 12 },
+    {date:'2024-05-14', day: 'Fri', slots: 21 },
+    {date:'2024-05-15', day: 'Today', slots: 3 },
+    {date:'2024-05-16', day: 'Today', slots: 3 },
+  ]
+export let data = [
+    {
+      'slots': slots,
+      "Provider ID": "020024",
+      "Hospital Name": "CENTRAL PENINSULA GENERAL HOSPITAL",
+      "Address": "250 HOSPITAL PLACE",
+      "City": "SOLDOTNA",
+      "State": "Alaska",
+      "ZIP Code": 99669,
+      "County Name": "",
+      "Phone Number": 9072624404,
+      "Hospital Type": "Acute Care Hospitals",
+      "Hospital Ownership": "Voluntary non-profit - Other",
+      "Emergency Services": "Yes",
+      "Meets criteria for meaningful use of EHRs": "Y",
+      "Hospital overall rating": 3,
+      "Hospital overall rating footnote": "",
+      "Mortality national comparison": "Same as the national average",
+      "Mortality national comparison footnote": "",
+      "Safety of care national comparison": "Same as the national average",
+      "Safety of care national comparison footnote": "",
+      "Readmission national comparison": "Same as the national average",
+      "Readmission national comparison footnote": "",
+      "Patient experience national comparison": "Same as the national average",
+      "Patient experience national comparison footnote": "",
+      "Effectiveness of care national comparison": "Same as the national average",
+      "Effectiveness of care national comparison footnote": "",
+      "Timeliness of care national comparison": "Below the national average",
+      "Timeliness of care national comparison footnote": "",
+      "Efficient use of medical imaging national comparison": "Same as the national average",
+      "Efficient use of medical imaging national comparison footnote": ""
+    }
+  ]
+  
+  
