10e10163c56c6df5c6a1882c5cd4468b18087c50
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-05-20 13:57:26 +0530

    Nav bar menu done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -31,9 +31,20 @@ export default function Navbar({ children, transperent = 0 }) {
             </IconButton>}
 
             {expand&&<div className={style.sideManu}>
-                <IconButton sx={{width:'50px'}} onClick={()=>{setExpand(false)}}>
+                <IconButton sx={{height:'50px',width:'100%',justifyContent:'flex-end',padding:2 }} onClick={()=>{setExpand(false)}}>
                     <Cancel/>
                 </IconButton>
+                <div className={style.manulist}>
+                <Link to="/SearchResult" className={style.span}>Find Doctors</Link>
+                <Link className={style.span}>Hospitals</Link>
+                <span className={style.span}>Medicines</span>
+                <span className={style.span}>Surgeries</span>
+                <span className={style.span}>Software for Provider</span>
+                <span className={style.span}>Facilities</span>
+                <div style={{height:'42px'}}>
+                <Button varient={btnVarient.LIGHTBLUE} handleClick={() => { console.log('My Bookings btn'); navigate('/MyBooking') }} btnText={'My Bookings'}></Button>
+                </div>
+            </div>
                 </div>}
         </div>
     )
